263553fb6b844c28ff5660f55a33cf43bb53135b
Vineet Tomar <tomarvineet949@gmail.com>
2026-09-06 00:55:13 +0530

    Python Fundamentals firs commit variables

---
+++ basic_calculations.py
@@ -0,0 +1,7 @@
+salary= 50000
+expense= 23010
+
+monthly_savings = salary - expense
+yearly_savings = monthly_savings*12
+print("Monthly Savings:", monthly_savings)
+print("Yearly Savings:", yearly_savings)
---
+++ variables.py
@@ -0,0 +1,19 @@
+name = "vineet"
+age = 25
+salary = 5000.23
+is_working = True
+
+# name = "vineet",
+# age = 25,
+# salary = 5000.23,   (jiske bhoi aage , lga hai uska type TUPLE aaega, becuase , TUPLES ke liye use hota hai)
+# is_working = True
+
+print("Name:",name)
+print("Age:", age)
+print("Salary", salary)
+print("Working:", is_working)
+
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(is_working))
