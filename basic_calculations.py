1a87054cfe57cde4b81fe2660965ec4be3b085ea
Vineet Tomar <tomarvineet949@gmail.com>
2026-09-06 10:34:13 +0530

    added saved percentage per month

--- basic_calculations.py
+++ basic_calculations.py
@@ -3,5 +3,7 @@ expense= 23010
 
 monthly_savings = salary - expense
 yearly_savings = monthly_savings*12
+save_perc =     (monthly_savings / salary) * 100
 print("Monthly Savings:", monthly_savings)
 print("Yearly Savings:", yearly_savings)
+print("Savings Percent:", save_perc)
