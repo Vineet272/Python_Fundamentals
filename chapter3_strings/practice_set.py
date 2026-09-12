0041f808cded3ac5616326b728ededc06a12b70c
Vineet Tomar <tomarvineet949@gmail.com>
2026-09-13 01:50:15 +0530

    added strings practice set file also

---
+++ chapter3_strings/practice_set.py
@@ -0,0 +1,14 @@
+# 1. to display name entred by user.
+
+#fSTRINGS
+# name = input("Enter your name: ")
+# print(f"Good Afternoon {name}")
+
+# 2. program to detect double space in a string.
+str = "hello  kese ho"
+# result = str.find("  ")
+# print("contains double space: ", result)
+
+# 3. replace double space with single space
+new_str = str.replace("  "," ")
+print(new_str)
