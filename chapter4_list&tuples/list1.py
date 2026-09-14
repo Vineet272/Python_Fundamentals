ba55a19b34cd944f688f5e6a0ad89ee2ee786bad
Vineet Tomar <tomarvineet949@gmail.com>
2026-09-14 23:35:58 +0530

    basic intro of list in python

---
+++ chapter4_list&tuples/list1.py
@@ -0,0 +1,9 @@
+#list is used to store multiple types of items in a single variable
+#LIST ARE MUTABLE   
+
+list = ["apple","banana", 1, True, 7.44]
+print(list)
+
+list[1] = "orange"   #it will change the element at 1 index in list
+print(list)
+
