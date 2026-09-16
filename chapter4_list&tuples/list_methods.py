57e0eceeba2fde85a58dd6973f7b906ee85403c9
Vineet Tomar <tomarvineet949@gmail.com>
2026-09-17 00:45:20 +0530

    added more important list methods

--- chapter4_list&tuples/list_methods.py
+++ chapter4_list&tuples/list_methods.py
@@ -15,4 +15,18 @@ print(list1)
 
 #remove() removes the specified value from the list
 # list1.remove(8.44)
-print(list1)
+
+#extend() to insert multiple values. append() will make a list inside list if we try to add multiple values
+# list1.extend([1,2,3])
+
+#index() find the index of given value, error if value not present
+# print(list1.index(5))
+
+#count() print the occurence of given value in a list
+# print(list1.count(5))
+
+#copy() to creat a shallow copy of a list
+# list2 = list1.copy()
+# print(list2)
+
+# print(list1)
