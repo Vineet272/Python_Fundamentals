47a15128e06e6bf9eeb7e21dfba70badfc93a81a
Vineet Tomar <tomarvineet949@gmail.com>
2026-09-17 00:20:01 +0530

    commonly used list methods

---
+++ chapter4_list&tuples/list_methods.py
@@ -0,0 +1,18 @@
+list1 = ["Apple", "Orange", 5, 8.44, False, "Harsh"]
+print(list1)
+
+# append() insert and object at the end of the list
+# list1.append("Vineet")
+
+# reverse() arrange list items in reverse order
+# list1.reverse()
+
+# insert() to insert an element in list at specified index(it is mandatory)
+# list1.insert(1, 5) #insert 5 at 1st index
+
+#pop() delets an element from specified index or default last index
+# list1.pop()
+
+#remove() removes the specified value from the list
+# list1.remove(8.44)
+print(list1)
